4f8f61bdb1c9b325a27cb3c7f7417c2d1f233517
brunovsiq <brunovsiq@gmail.com>
2018-08-30 14:28:44 -0300

    [Map] Show blue dot icon to user locations, and starting retrieving all user locations

--- app/src/main/java/br/com/infracea/fiscalapp/screens/container/ContainerActivity.java
+++ app/src/main/java/br/com/infracea/fiscalapp/screens/container/ContainerActivity.java
@@ -1,6 +1,5 @@
 package br.com.infracea.fiscalapp.screens.container;
 
-import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -9,25 +8,21 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.util.Arrays;
 
 import br.com.infracea.fiscalapp.R;
 import br.com.infracea.fiscalapp.basic.BasicActivity;
@@ -36,7 +31,6 @@ import br.com.infracea.fiscalapp.screens.container.chat.ChatFragment;
 import br.com.infracea.fiscalapp.screens.container.map.MapFragment;
 import br.com.infracea.fiscalapp.screens.container.menu.MenuFragment;
 import br.com.infracea.fiscalapp.screens.login.HomeActivity;
-import br.com.infracea.fiscalapp.screens.login.LoginActivity;
 import br.com.infracea.fiscalapp.util.GPS.GPSLocation;
 import br.com.infracea.fiscalapp.util.GPS.LocationService;
 
@@ -130,14 +124,8 @@ public class ContainerActivity extends BasicActivity {
 
     public void locationUpdated(Location location) {
 //        Log.d("CONTAINER", "Camera Moved New Location: " + location.toString());
-//        if (mapFragment.lastUserLocation != null) {
-//            mapFragment.lastUserLocation = location;
-//        } else {
-//            mapFragment.lastUserLocation = location;
-//            mapFragment.moveCamera(location);
-//        }
         mapFragment.lastUserLocation = location;
-        mapFragment.moveCamera(location);
+        //mapFragment.moveMarker(location);
 
     }
 
@@ -146,8 +134,7 @@ public class ContainerActivity extends BasicActivity {
         //verificar se deu ou nao a permissao, se deu, passa de tela
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1) {
-            if (ActivityCompat.checkSelfPermission(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 this.startService(new Intent(this, LocationService.class));
             } else {
                 Toast.makeText(this, "É necessário aceitar a permissão de localização!", Toast.LENGTH_LONG).show();
--- app/src/main/java/br/com/infracea/fiscalapp/screens/container/map/MapFragment.java
+++ app/src/main/java/br/com/infracea/fiscalapp/screens/container/map/MapFragment.java
@@ -1,12 +1,11 @@
 package br.com.infracea.fiscalapp.screens.container.map;
 
+import android.Manifest;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
@@ -14,27 +13,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQuery;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import br.com.infracea.fiscalapp.R;
-import br.com.infracea.fiscalapp.screens.login.HomeActivity;
 import br.com.infracea.fiscalapp.util.GPS.GPSLocation;
-import br.com.infracea.fiscalapp.util.GPS.LocationService;
 
 public class MapFragment extends Fragment {
 
+
+
     View view;
+
     private SupportMapFragment mapFragment;
     private MapView mapView;
     private FloatingActionButton floatingActionButton;
@@ -44,19 +48,20 @@ public class MapFragment extends Fragment {
     private Marker m;
 
     public Location lastUserLocation;
+    private GeoQuery geoQuery;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        floatingActionButton =  view.findViewById(R.id.frag_map_userlocation_button);
+        floatingActionButton = view.findViewById(R.id.frag_map_userlocation_button);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (googleMap != null && lastUserLocation != null) {
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastUserLocation.getLatitude(), lastUserLocation.getLongitude()), 14.0f));
-                    moveCamera(lastUserLocation);
+                    moveCamera();
+                    //moveMarker(lastUserLocation);
                 }
             }
         });
@@ -80,6 +85,14 @@ public class MapFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (lastUserLocation != null) {
+            loadUsersMarkers(lastUserLocation);
+        }
+    }
+
     private void moveCameraToUserLocation() {
         final ProgressDialog pd = new ProgressDialog(getContext());
         pd.setMessage("Atualizando sua localização");
@@ -89,53 +102,44 @@ public class MapFragment extends Fragment {
             @Override
             public void run() {
                 if (googleMap != null && lastUserLocation != null) {
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastUserLocation.getLatitude(), lastUserLocation.getLongitude()), 14.0f));
-                    moveCamera(lastUserLocation);
+                    moveCamera();
+                    //moveMarker(lastUserLocation);
                     pd.dismiss();
                 }
             }
         }, 2000);
     }
 
+    private void moveCamera() {
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lastUserLocation.getLatitude(), lastUserLocation.getLongitude()), 17.0f));
+    }
+
     OnMapReadyCallback onMapReady = new OnMapReadyCallback() {
         @Override
         public void onMapReady(GoogleMap map) {
             googleMap = map;
-//            map.addMarker(new MarkerOptions()
-//                    .position(new LatLng(event.getAddress().getLatitude(), event.getAddress().getLongitude()))
-//                    .title(event.getAddress().getName()));
-//
-            final LatLng LOCAL = new LatLng( -15.7801, -47.9292);
-//
-////            map.setMinZoomPreference(100.0f);
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
+                return;
+            }
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+//            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                googleMap.setMyLocationEnabled(true);
+//                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+//            } else {
+//
+//            }
 
-//            CameraUpdateFactory.newLatLngZoom( new LatLng(event.getArea().getLatitude(), event.getArea().getLongitude()) , 14.0f );
         }
     };
 
-    public void moveCamera(Location mLastLocation) {
-        try {
+    public void loadUsersMarkers(Location userLocation) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("user_locations");
+        GeoFire geoFire = new GeoFire(ref);
 
-            if (!markerAdded) {
-                mp.position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
-                //mp.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
-                mp.title("Minha posição");
-                //googleMap.addMarker(mp);
-                m = googleMap.addMarker(mp);
-                markerAdded = true;
-            }
-
-
-            m.setPosition(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
-
-            //mp.position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
-
-
-
-        } catch (NullPointerException e) {
-            //nada-faz
-        }
+        geoQuery = geoFire.queryAtLocation(new GeoLocation(userLocation.getLatitude(),userLocation.getLongitude()), 10.0);
     }
 
 }
